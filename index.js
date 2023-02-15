8fe48b70ce99c6759f54b16057bfcfda98b2d3db
GOKULAKRISHNAN . J <110898672+gokulakrishnan-j@users.noreply.github.com>
2023-02-15 21:14:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-import array from "./amazonScrap.js"; 
->>>>>>> e740ef2e43d76f7034f5437bedaef73b68e66ffe
 import express from "express";
 import { MongoClient } from "mongodb";
 import cors from "cors"
@@ -33,7 +29,7 @@ async function genHashedPassword (password){
 app.use(express.json())
 app.use(cors())
 const PORT = process.env.PORT
-<<<<<<< HEAD
+
 
 
 const array = []
@@ -41,9 +37,6 @@ const array = []
 request('https://www.amazon.in/s?k=all&crid=3KH4GE89Z7E0Q&sprefix=%2Caps%2C484&ref=nb_sb_ss_recent_1_0_recent',(err,response,html)=>{
 
     const $= cheerio.load(html)
-
-=======
->>>>>>> e740ef2e43d76f7034f5437bedaef73b68e66ffe
 
 
     $('.sg-col-4-of-20') 
@@ -81,59 +74,30 @@ request('https://www.amazon.in/s?k=all&crid=3KH4GE89Z7E0Q&sprefix=%2Caps%2C484&r
 })
 
 
-app.get('/',auth,async function(request,response){
 
 
-
-<<<<<<< HEAD
-    const deleting = await client
-=======
 
 app.get('/',async function(request,response){
 
     const deletes = await client
->>>>>>> e740ef2e43d76f7034f5437bedaef73b68e66ffe
     .db('E-commerce')
     .collection('product')
     .deleteMany() 
 
-<<<<<<< HEAD
     const result = await client
     .db('E-commerce')
     .collection('product')
     .insertMany(array)
     
-=======
- names()
->>>>>>> e740ef2e43d76f7034f5437bedaef73b68e66ffe
+
     response.send("done")
     })
 
 
-<<<<<<< HEAD
 app.use("/product",productRouter)
 app.use("/user",userRouter)
-=======
-app.get('/product',async function(request,response){
-    
- const result = await client
-.db('E-commerce')
-.collection('product')
-.find({})
-.toArray();
-response.send(result)
-})
 
-app.get("/product/:name",async function(request,response){
-const {name}=request.params
-    const search = await client
-    .db('E-commerce')
-    .collection('product')
-    .find({title:{$regex:name.replace(/yes/g,'')}})
-    .toArray()
-   search ? response.send(search) : response.status(400).send(`product not available:${name}`)
-})
->>>>>>> e740ef2e43d76f7034f5437bedaef73b68e66ffe
+
 
 
 app.listen(PORT)
