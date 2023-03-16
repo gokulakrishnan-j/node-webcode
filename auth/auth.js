0f75afa41fe1586f5e04b94092a6cab062748442
GOKULAKRISHNAN . J <gokulakrishnan252@gmail.com>
2023-03-16 10:07:43 +0530

    Update auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken"
+import * as dotenv from "dotenv"
+dotenv.config()
 
 export const auth = (request,response,next)=>{
     try{
         
         const token = request.header("my_token");
         
-        jwt.verify(token,process.env.SECRET_KEY);
+        jwt.verify(token,process.env.SECERET_KEY);
         
         next();
     }catch (err) {
