a739ccd5cb2c4deb865ad133371dc1a943a2c116
GOKULAKRISHNAN . J <gokulakrishnan252@gmail.com>
2023-03-17 13:57:16 +0530

    Delete produt.service.js

--- service/produt.service.js
+++
@@ -1,9 +0,0 @@
-import { client } from '../index.js';
-
-export async function resuting() {
-    return await client
-        .db('E-commerce')
-        .collection('product')
-        .find({})
-        .toArray();
-}
